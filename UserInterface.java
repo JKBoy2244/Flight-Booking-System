17629d70c9fd43c0a172f8cf68c73533ce589c4c
JKBoy2244 <jobayer2006@gmail.com>
2025-12-19 21:41:56 +0000

    Create UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,9 @@
+interface UserInterface {
+  public void firstName();
+  public void lastName();
+  public void age();
+  public void gender();
+  public void emailAddress();
+  public void phoneNumber();
+  public void confirm();
+}
