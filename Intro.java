d9ac0bf8d472baeedbafcc34d5139d77b0841938
JKBoy2244 <jobayer2006@gmail.com>
2025-12-19 21:42:26 +0000

    Create Intro.java

---
+++ Intro.java
@@ -0,0 +1,11 @@
+class Intro {
+
+  public void displayMessage() {
+
+    System.out.println("Welcome to the Flight Booking Reservation System!"); 
+    System.out.println("Remeber to book your flights in advance!"); 
+    System.out.println("Also, this system is a super strong security systems. You have 3 attempts to enter each information correctly and if you fail to do so, you'll get locked out and have to start again. This is to prevent any unauthorized attacks. Thank you!");
+    System.out.println("Please enter each information accurately and correctly as once you enter each piece of information, you can't edit/change it again otherwise you'd have to restart from beginning as this is a super strong secure system!");
+
+  }
+}
