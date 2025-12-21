1ca7a8593dce80d85524780cde81c1ad2eb47fd0
JKBoy2244 <jobayer2006@gmail.com>
2025-12-21 22:29:01 +0000

    Update FlightInformation.java

--- Classes/FlightInformation.java
+++ Classes/FlightInformation.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
 class FlightInformation extends UserInformation implements UserInterface {
 
   private static final int maxAttempts = 3;
