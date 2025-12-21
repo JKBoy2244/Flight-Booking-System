1f90de9c25fe2300bbc049520d3912b2b55e7a66
JKBoy2244 <jobayer2006@gmail.com>
2025-12-21 22:31:01 +0000

    Update IllegalFlightSeatNumberException.java

--- Exceptions/IllegalFlightSeatNumberException.java
+++ Exceptions/IllegalFlightSeatNumberException.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
 public class IllegalFlightSeatNumberException extends IllegalArgumentException {
     public IllegalFlightSeatNumberException(String message) {
         super(message);
