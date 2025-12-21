144923a8f707e95ac33f752c9dad0853bf2a8e19
JKBoy2244 <jobayer2006@gmail.com>
2025-12-21 22:31:23 +0000

    Update IllegalGenderException.java

--- Exceptions/IllegalGenderException.java
+++ Exceptions/IllegalGenderException.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
 public class IllegalGenderException extends IllegalArgumentException {
     public IllegalGenderException(String message) {
         super(message);
