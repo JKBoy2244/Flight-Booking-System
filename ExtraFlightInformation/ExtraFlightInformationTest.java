0676783f196e826d3da8d3cb210311d323a5c0aa
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 12:00:00 +0000

    Update ExtraFlightInformationTest.java

--- ExtraFlightInformation/ExtraFlightInformationTest.java
+++ ExtraFlightInformation/ExtraFlightInformationTest.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 
-class ExtraFlightInformationTest {
+public class ExtraFlightInformationTest {
 
    public static void main(String[] args) {
 
