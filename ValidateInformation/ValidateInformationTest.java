acb8ba0b30dc220d326163abfb1b750177b1f4f9
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 12:58:58 +0000

    Update ValidateInformationTest.java

--- ValidateInformation/ValidateInformationTest.java
+++ ValidateInformation/ValidateInformationTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import classes.ValidateInformation;
+import Exceptions.*;
 
 
 public class ValidateInformationTest {
