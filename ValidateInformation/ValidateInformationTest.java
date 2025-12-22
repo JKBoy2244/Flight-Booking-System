cc94c33b8ea4be227a7457a546fd667dce2fcc1a
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 13:09:55 +0000

    Update ValidateInformationTest.java

--- ValidateInformation/ValidateInformationTest.java
+++ ValidateInformation/ValidateInformationTest.java
@@ -12,6 +12,10 @@ import ExtraFlightInformation.ExtraFlightInformation;
 
 public class ValidateInformationTest {
     public static void main(String[] args) {
+
+        FlightInformation startFlights = new FlightInformation("Bob", "Mitchell" ,70, "Male" ,"BobMitchell@gmail.com", "0123456789", "Seoul", "01/07/2025", "04:00", "402"); //For reference, this is made up data.
+        ExtraFlightInformation endFlights = new ExtraFlightInformation("05:30", "09:30", "324", 1);
+        
         ValidateInformation validate = new ValidateInformation();
 
         System.out.println("ValidateInformationTest ran.");
