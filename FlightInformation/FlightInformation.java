a40b12df7a11aa7603e2ddd576dc1b149c007e5d
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 13:03:03 +0000

    Update FlightInformation.java

--- FlightInformation/FlightInformation.java
+++ FlightInformation/FlightInformation.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import Exceptions.*;
-
+import Interface.UserInterface;
 
 public class FlightInformation extends UserInformation implements UserInterface {
 
