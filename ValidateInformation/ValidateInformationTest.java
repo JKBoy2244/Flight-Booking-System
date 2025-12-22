c9389c8d8e934d40353cb473e365760a6ee4548a
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 12:06:46 +0000

    Update ValidateInformationTest.java

--- ValidateInformation/ValidateInformationTest.java
+++ ValidateInformation/ValidateInformationTest.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import classes.ValidateInformation;
 
 
 public class ValidateInformationTest {
-  
     public static void main(String[] args) {
+        ValidateInformation validate = new ValidateInformation();
 
-       ValidateInformation validate = new ValidateInformation(); 
-    }  
+        System.out.println("ValidateInformationTest ran.");
+    }
 }
