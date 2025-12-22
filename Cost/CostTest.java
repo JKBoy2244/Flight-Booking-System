7f0f9316fbd95521f02a595276003513dfbe5b37
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 12:03:04 +0000

    Update CostTest.java

--- Cost/CostTest.java
+++ Cost/CostTest.java
@@ -1,1 +1,16 @@
+package Cost;
 
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
+class CostTest {
+
+  public static void main(String[] args) {
+
+    Cost cost = new Cost(1.20);
+  }
+}
