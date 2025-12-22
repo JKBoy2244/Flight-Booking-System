866e40a6d4bc042098922ad7dfa9d7bb6131312f
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 16:01:50 +0000

    Update FlightInformationTest.java

--- FlightInformation/FlightInformationTest.java
+++ FlightInformation/FlightInformationTest.java
@@ -11,7 +11,17 @@ public class FlightInformationTest {
 
   public static void main(String[] args) {
     
-      FlightInformation startFlights = new FlightInformation("Bob", "Mitchell" ,70, "Male" ,"BobMitchell@gmail.com", "0123456789", "Seoul", "01/07/2025", "04:00", "402"); //For reference, this is made up data.
+      FlightInformation startFlights = new FlightInformation("Bob", "Mitchell" ,70, "Male" ,"BobMitchell@gmail.com", "01234567893", "Seoul", "01/07/2025", "04:00", "402"); //For reference, this is made up data.
+      System.out.println(startFlights.getFirstName());
+      System.out.println(startFlights.getLastName());
+      System.out.println(startFlights.getAge());
+      System.out.println(startFlights.getGender());
+      System.out.println(startFlights.getEmailAddress());
+      System.out.println(startFlights.getPhoneNumber());
+      System.out.println(startFlights.getFlightDestination());
+      System.out.println(startFlights.getFlightDate());
+      System.out.println(startFlights.getFlightTime());
+      System.out.println(startFlights.getFlightSeatNumber());
 
   }
 }
