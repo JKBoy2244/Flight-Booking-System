dac897a6de577156ec4f822f61b3ede5cad3dac2
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 11:58:05 +0000

    Update ExtraFlightInformationTest.java

--- ExtraFlightInformation/ExtraFlightInformationTest.java
+++ ExtraFlightInformation/ExtraFlightInformationTest.java
@@ -1,1 +1,17 @@
+package ExtraFlightInformation;
 
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
+class ExtraFlightInformationTest {
+
+   public static void main(String[] args) {
+
+     ExtraFlightInformation endFlights = new ExtraFlightInformation("05:30", "09:30", "324", 1);
+     
+   }
+}
