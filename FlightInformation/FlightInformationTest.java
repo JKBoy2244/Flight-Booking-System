d47c5b8093acc7320904ee7a8cfec1f1ff98b443
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 12:28:48 +0000

    Update FlightInformationTest.java

--- FlightInformation/FlightInformationTest.java
+++ FlightInformation/FlightInformationTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class FlightInformationTest {
 
   public static void main(String[] args) {
-.
+    
       FlightInformation startFlights = new FlightInformation("Bob", "Mitchell" ,70, "Male" ,"BobMitchell@gmail.com", "0123456789", "Seoul", "01/07/2025", "04:00", "402"); //For reference, this is made up data.
 
   }
