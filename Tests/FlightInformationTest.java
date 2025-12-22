50daedb2c4556b2e7161b0fbcfb73913d327bd3d
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 11:40:00 +0000

    Update FlightInformationTest.java

--- Tests/FlightInformationTest.java
+++ Tests/FlightInformationTest.java
@@ -1,1 +1,15 @@
+package Tests;
 
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class FlightInformationTest {
+
+  public static void main(String[] args) {
+.
+      FlightInformation startFlights = new FlightInformation("Bob", "Mitchell" ,70, "Male" ,"BobMitchell@gmail.com", "0123456789", "Seoul", "01/07/2025", "04:00", "402"); //For reference, this is made up data.
+
+  }
+}
