860853953f89863ff2cb4f2be15bc92cbebaccb0
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:20:01 +0000

    Create CostTest2.java

---
+++ Cost/CostTest2.java
@@ -0,0 +1,19 @@
+package Cost;
+
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import Exceptions.*;
+import ValidateInformation.ValidateInformation;
+import FlightInformation.UserInformation;
+
+public class CostTest2 {
+
+  public static void main(String[] args) {
+
+    Cost cost = new Cost("er34sd");
+    System.out.println(cost.getTotalCost());
+  }
+}
