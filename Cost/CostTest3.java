c01eef3f17c8809a291b6a231ae4d1a1e9406cd2
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:20:23 +0000

    Create CostTest3.java

---
+++ Cost/CostTest3.java
@@ -0,0 +1,19 @@
+package Cost;
+
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import Exceptions.*;
+import ValidateInformation.ValidateInformation;
+import FlightInformation.UserInformation;
+
+public class CostTest3 {
+
+  public static void main(String[] args) {
+
+    Cost cost = new Cost(0.00);
+    System.out.println(cost.getTotalCost());
+  }
+}
