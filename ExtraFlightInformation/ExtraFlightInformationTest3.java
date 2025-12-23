7438ad8b03d5687b233c9a85c1a6209cfacfce1a
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:13:48 +0000

    Update ExtraFlightInformationTest3.java

--- ExtraFlightInformation/ExtraFlightInformationTest3.java
+++ ExtraFlightInformation/ExtraFlightInformationTest3.java
@@ -1,1 +1,21 @@
+package ExtraFlightInformation;
 
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import Exceptions.*;
+
+
+public class ExtraFlightInformationTest3 {
+
+   public static void main(String[] args) {
+
+     ExtraFlightInformation endFlights = new ExtraFlightInformation("12/67/2099", "09:37", "8544", 0);
+     System.out.println(endFlights.getReturnFlightDate());
+     System.out.println(endFlights.getReturnFlightTime());
+     System.out.println(endFlights.getReturnFlightSeatNumber());
+
+   }
+}
