f3e95246aeccd6b43ebc79816c088fb37151cb52
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:17:22 +0000

    Update and rename ValidateInformationTest.java to ValidateInformationTest1.java

--- ValidateInformation/ValidateInformationTest1.java
+++ ValidateInformation/ValidateInformationTest1.java
@@ -10,7 +10,7 @@ import FlightInformation.FlightInformation;
 import ExtraFlightInformation.ExtraFlightInformation;
 
 
-public class ValidateInformationTest {
+public class ValidateInformationTest1 {
     public static void main(String[] args) {
 
         FlightInformation startFlights = new FlightInformation("Bob", "Mitchell" ,70, "Male" ,"BobMitchell@gmail.com", "0123456789", "Seoul", "01/07/2025", "04:00", "402"); //For reference, this is made up data.
