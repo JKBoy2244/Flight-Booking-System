1aae924fe0fce98c3c0f81abd8b6477f9747cac5
JKBoy2244 <jobayer2006@gmail.com>
2026-01-29 21:19:09 +0000

    Update ValidateInformationTest2.java

--- ValidateInformation/ValidateInformationTest2.java
+++ ValidateInformation/ValidateInformationTest2.java
@@ -1,4 +1,6 @@
 package ValidateInformation;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 import java.util.Scanner;
@@ -11,7 +13,8 @@ import ExtraFlightInformation.ExtraFlightInformation;
 
 
 public class ValidateInformationTest2 {
-    public static void main(String[] args) {
+    @Test
+    void constructor_sets_all_fields_correctly() {
 
         FlightInformation startFlights = new FlightInformation("", "" ,7056666, "Male" ,"", "0123456789566tgfd", "Seoul", "01/07/2025", "04:00", "572"); //For reference, this is made up data.
         ExtraFlightInformation endFlights = new ExtraFlightInformation("05:30", "", "724", 1);
